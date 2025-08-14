a8e09b69ff985c4425895658e740621133399ac8
eidelo <80028270+eidelo@users.noreply.github.com>
2025-08-14 11:43:15 +0200

    Add files via upload
    Used to organize protein and genome sequences downloaded from NCBI datasets

---
+++ file_organizer.py
@@ -0,0 +1,62 @@
+from pathlib import Path
+import argparse
+from tqdm import tqdm
+from Bio import SeqIO
+from typing import List
+
+def rm_tree(pth: Path):
+    for child in pth.iterdir():
+        if child.is_file():
+            child.unlink()
+        else:
+            rm_tree(child)
+    pth.rmdir()
+
+
+parser = argparse.ArgumentParser(
+                    prog="organiser.py",
+                    description="Organise files downloaded from NCBI datasets"
+                    )
+parser.add_argument("indir", help="Input directory (after unzip)")
+parser.add_argument("outdir", help="Name of output directory")
+args = parser.parse_args()
+
+indir = Path(args.indir)
+outdir = Path(args.outdir)
+data_dir = indir / "data"
+
+if not data_dir.exists():
+    raise FileNotFoundError("Input directory doesn't contain 'data' subdirectory")
+
+subdirs = [subdir for subdir in data_dir.glob("*") if subdir.is_dir()]
+
+# Create output directories and organize
+outdir.mkdir(exist_ok=True)
+file_types = ["protein"]
+for subdir in tqdm(subdirs, desc="Organizing files"):
+    input_files = subdir.glob("*")
+    assembly = subdir.stem
+    for f in input_files:
+        for file_type in file_types:
+            if file_type not in f.stem:
+                continue
+            file_type_dir = outdir / file_type
+            file_type_dir.mkdir(exist_ok=True)
+            new_name = assembly + f.suffix
+            f.rename(file_type_dir / new_name)
+
+# Remove input directory
+rm_tree(indir)
+readme_file = outdir / "README.md"
+readme_file.unlink()
+
+files = list(file_type_dir.glob("*"))
+for f in tqdm(files, desc="Renaming proteins for downstream analysis"):
+    data = []
+    parser = SeqIO.parse(f, "fasta")
+    gcf = f.stem
+    for record in parser:
+        record.id = record.id + "-" + gcf
+        record.description = ""
+        data.append(record)
+    SeqIO.write(data, f, "fasta")
